b90ce014abc612bfa2c5e9eb21028e61ce8fb13c
mwdziak <mwdziak@gmail.com>
2024-09-01 13:43:47 +0200

    Defined user entity and factory function

--- entities.go
+++ entities.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type UserEntity struct {
+	Id string `bson:"id" json:"id"`
+	Username string `bson:"username" json:"username"`
+	Email string `bson:"email" json:"email"`
+	Password string `bson: "password" json:"password"`
+}
 
 type ChatRoomEntity struct {
 	Id       string          `bson:"id" json:"id"`
@@ -21,4 +31,18 @@ func NewMessageEntity(content string, chatRoomId string) *MessageEntity {
 		Content:  content,
 		ChatRoomId: chatRoomId,
 	}
+}
+
+func NewUserEntity(username string, email string, password string) *UserEntity {
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return &UserEntity{
+        Id:       uuid.New().String(),
+        Username: username,
+        Email:    email,
+        Password: string(hashedPassword),
+    }
 }
