e0bacbe5c559838289f8ac028d0a2bbdf18cf650
mwdziak <mwdziak@gmail.com>
2024-09-19 19:39:52 +0200

    Created Message struct that will serve as a blueprint for serialization message with some metadata.

---
+++ room/structs.go
@@ -0,0 +1,24 @@
+package room
+
+import "time"
+
+type MessageType int
+
+const (
+	TextMessage MessageType = iota
+	ImageMessage
+)
+
+type UserDetails struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type Message struct {
+	Id     string      `json:"id"`
+	Type MessageType `json:"messageType"`
+	Content string `json:"content"`
+	SentBy UserDetails `json:"sentBy"`
+	SentAt time.Time `json:"sentAt"`
+	SeenBy []UserDetails `json:"seenBy"`
+}
