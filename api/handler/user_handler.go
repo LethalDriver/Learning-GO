a206e7622f8efcb9c447b83ab497c410da45dcae
mwdziak <mwdziak@gmail.com>
2024-10-06 12:51:54 +0200

    Improved error messages in handle login.

--- api/handler/user_handler.go
+++ api/handler/user_handler.go
@@ -25,7 +25,7 @@ func (h *UserHandler) HandleRegister(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	var regReq service.RegistrationRequest
-    err := parseRequest(r, &regReq)
+	err := parseRequest(r, &regReq)
 	if err != nil {
 		log.Printf("Failed registering user %q: %v", regReq.Username, err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
@@ -58,15 +58,21 @@ func (h *UserHandler) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	var logReq service.LoginRequest
 	err := parseRequest(r, &logReq)
 	if err != nil {
-		log.Printf("Failed logging in user %q: %v", logReq.Username, err)
+		log.Printf("Failed parsing login request: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	token, err := h.s.LoginUser(ctx, logReq)
 	if err != nil {
-		log.Printf("Failed logging in user %q: %v", logReq.Username, err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		switch err {
+		case service.ErrNoUser:
+			http.Error(w, "User not found", http.StatusNotFound)
+		case service.ErrWrongPassword:
+			http.Error(w, "Incorrect password", http.StatusUnauthorized)
+		default:
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
 		return
 	}
 
@@ -75,7 +81,7 @@ func (h *UserHandler) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	}
 	err = writeResponse(w, resp)
 	if err != nil {
-		log.Printf("Failed logging in user %q: %v", logReq.Username, err)
+		log.Printf("Failed writing login response: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
