51e997af29d4b2d8649e55f3cbd5e6e482d2ab3c
mwdziak <mwdziak@gmail.com>
2024-10-06 11:02:33 +0200

    Handle connection function doesn't create a new chat room anymore, this operation will be handled by the room service.

--- service/connection_service.go
+++ service/connection_service.go
@@ -1,9 +1,7 @@
 package service
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"sync"
 
@@ -12,137 +10,138 @@ import (
 )
 
 type Connection struct {
-    ws   *websocket.Conn
-    user structs.UserDetails
-    sendMessage chan structs.Message
+	ws             *websocket.Conn
+	user           structs.UserDetails
+	sendMessage    chan structs.Message
 	sendSeenUpdate chan structs.SeenUpdate
-    room *ChatRoom
+	room           *ChatRoom
 }
 
-func HandleConnection(ws *websocket.Conn, s *RoomService, roomId string, user structs.UserDetails) error {
-    ctx := context.Background()
+func HandleConnection(ws *websocket.Conn, room *ChatRoom, user structs.UserDetails) error {
+	clientIP := ws.RemoteAddr().String()
+	log.Printf("Handling connection from %s", clientIP)
 
-    log.Println("Handling connection")
-    conn := &Connection{
-        ws:   ws,
-        user: user,
-        sendMessage: make(chan structs.Message, 256),
+	conn := &Connection{
+		ws:             ws,
+		user:           user,
+		sendMessage:    make(chan structs.Message, 256),
 		sendSeenUpdate: make(chan structs.SeenUpdate, 256),
-    }
+		room:           room,
+	}
 
-    room, err := s.GetOrCreateRoom(ctx, roomId, conn)
-    if err != nil {
-        return fmt.Errorf("error creating room: %v", err)
-    }
-    conn.room = room
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-    var wg sync.WaitGroup
-    wg.Add(2)
+	go func() {
+		defer wg.Done()
+		conn.writePump()
+	}()
 
-    go func() {
-        defer wg.Done()
-        conn.writePump()
-    }()
+	go func() {
+		defer wg.Done()
+		conn.readPump()
+	}()
 
-    go func() {
-        defer wg.Done()
-        conn.readPump()
-    }()
+	wg.Wait()
+	room.Unregister <- conn
+	log.Printf("Connection %s unregistered from room ID: %s", clientIP, room.Id)
 
-    wg.Wait()
-    room.Unregister <- conn
-    log.Printf("Connection unregistered from room ID: %s", roomId)
-
-    return nil
+	return nil
 }
 
 func (c *Connection) readPump() {
-    defer func() {
-        log.Println("Closing WebSocket connection in readPump")
-        c.ws.Close() // Close the WebSocket connection
-    }()
+	defer c.closeWebSocket("Closing WebSocket connection in readPump")
 
-    for {
-        _, messageBytes, err := c.ws.ReadMessage()
-        if err != nil {
-            log.Printf("Error reading message in readPump: %v", err)
-            break
-        }
-        log.Printf("Read message from connection: %q, address: %p", string(messageBytes), c)
+	for {
+		_, messageBytes, err := c.ws.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message in readPump: %v", err)
+			break
+		}
+		log.Printf("Read message from connection: %q, address: %p", string(messageBytes), c)
+
 		messageType, err := structs.DetermineDataType(messageBytes)
-        if err != nil {
-            log.Printf("Error determining message type: %v", err)
-            break
-        }
+		if err != nil {
+			log.Printf("Error determining message type: %v", err)
+			break
+		}
 
-        switch messageType {
-        case structs.StatusUpdate:
-            var seenUpdate structs.SeenUpdate
-            err = json.Unmarshal(messageBytes, &seenUpdate)
-            if err != nil {
-                log.Printf("Error unmarshalling SeenUpdate: %v", err)
-                break
-            }
+		switch messageType {
+		case structs.StatusUpdate:
+			var seenUpdate structs.SeenUpdate
+			if err := c.unmarshalMessage(messageBytes, &seenUpdate); err != nil {
+				break
+			}
 			seenUpdate.SeenBy = c.user
 			log.Printf("Received SeenUpdate: %+v", seenUpdate)
 			c.room.StatusUpdates <- seenUpdate
-        case structs.MessageWithContent:
-            var msg structs.Message
-            err = json.Unmarshal(messageBytes, &msg)
-            if err != nil {
-                log.Printf("Error unmarshalling Message: %v", err)
-                break
-            }
-            msg.SentBy = c.user
-            c.room.Broadcast <- msg
-            log.Printf("Received Message: %+v", msg)
-        default:
-            log.Printf("Unknown message type: %q", string(messageBytes))
-        }
-    }
-    log.Println("Exiting readPump")
+
+		case structs.MessageWithContent:
+			var msg structs.Message
+			if err := c.unmarshalMessage(messageBytes, &msg); err != nil {
+				break
+			}
+			msg.SentBy = c.user
+			c.room.Broadcast <- msg
+			log.Printf("Received Message: %+v", msg)
+
+		default:
+			log.Printf("Unknown message type: %q", string(messageBytes))
+		}
+	}
+	log.Println("Exiting readPump")
 }
 
 func (c *Connection) writePump() {
-    defer func() {
-        log.Println("Sending close message and closing WebSocket connection in writePump")
-        c.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-        c.ws.Close()
-    }()
+	defer c.closeWebSocket("Sending close message and closing WebSocket connection in writePump")
 
-    for {
-        select {
-        case message, ok := <-c.sendMessage:
-            if !ok {
-                log.Println("sendMessage channel closed")
-                return
-            }
-            messageBytes, err := json.Marshal(message)
-            if err != nil {
-                log.Println("Error marshalling message:", err)
-                return
-            }
-            if err := c.ws.WriteMessage(websocket.TextMessage, messageBytes); err != nil {
-                log.Printf("Error writing message in writePump: %v", err)
-                return
-            }
-            log.Printf("Wrote message to connection: %q, address: %p", string(messageBytes), c)
+	for {
+		select {
+		case message, ok := <-c.sendMessage:
+			if !ok {
+				log.Println("sendMessage channel closed")
+				return
+			}
+			if err := c.writeMessage(websocket.TextMessage, message); err != nil {
+				return
+			}
 
-        case seenUpdate, ok := <-c.sendSeenUpdate:
-            if !ok {
-                log.Println("seenUpdate channel closed")
-                return
-            }
-            seenUpdateBytes, err := json.Marshal(seenUpdate)
-            if err != nil {
-                log.Println("Error marshalling seen update:", err)
-                return
-            }
-            if err := c.ws.WriteMessage(websocket.TextMessage, seenUpdateBytes); err != nil {
-                log.Printf("Error writing seen update in writePump: %v", err)
-                return
-            }
-            log.Printf("Wrote seen update to connection: %q, address: %p", string(seenUpdateBytes), c)
-        }
-    }
+		case seenUpdate, ok := <-c.sendSeenUpdate:
+			if !ok {
+				log.Println("seenUpdate channel closed")
+				return
+			}
+			if err := c.writeMessage(websocket.TextMessage, seenUpdate); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (c *Connection) closeWebSocket(logMessage string) {
+	log.Println(logMessage)
+	c.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	c.ws.Close()
+}
+
+func (c *Connection) unmarshalMessage(messageBytes []byte, v interface{}) error {
+	err := json.Unmarshal(messageBytes, v)
+	if err != nil {
+		log.Printf("Error unmarshalling message: %v", err)
+	}
+	return err
+}
+
+func (c *Connection) writeMessage(messageType int, data interface{}) error {
+	messageBytes, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("Error marshalling message: %v", err)
+		return err
+	}
+	if err := c.ws.WriteMessage(messageType, messageBytes); err != nil {
+		log.Printf("Error writing message: %v", err)
+		return err
+	}
+	log.Printf("Wrote message to connection: %q, address: %p", string(messageBytes), c)
+	return nil
 }
