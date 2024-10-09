c37cf7eb7cba1e3351c4b946e244125b0d1f02b6
mwdziak <mwdziak@gmail.com>
2024-10-09 23:06:41 +0200

    Added mongo connection to main of image service

--- media-service/main.go
+++ media-service/main.go
@@ -14,7 +14,20 @@ func main() {
 		log.Println("No .env file found, assuming variables set at system level")
 	}
 
-	router := initializeRoutes() // configure routes
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB!")
+
+	router := initializeRoutes() 
 
 	port := os.Getenv("PORT")
 
