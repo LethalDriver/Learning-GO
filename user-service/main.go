8b58ae04f9f92fa3d2460f8a2587c99a609a649f
mwdziak <mwdziak@gmail.com>
2024-11-04 21:26:47 +0100

    Removed /api from the user service endpoints path, replaced them with /auth and /users.

--- user-service/main.go
+++ user-service/main.go
@@ -59,8 +59,8 @@ func main() {
 
 func initializeRoutes(u *handler.UserHandler) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /api/register", u.HandleRegister)
-	mux.HandleFunc("POST /api/login", u.HandleLogin)
-	mux.HandleFunc("GET /api/me", u.HandleMe)
+	mux.HandleFunc("POST auth/register", u.HandleRegister)
+	mux.HandleFunc("POST auth/login", u.HandleLogin)
+	mux.HandleFunc("GET users/me", u.HandleMe)
 	return mux
 }
