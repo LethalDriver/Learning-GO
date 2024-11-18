1e4ee953300360cd41568536d49a2b36cd505a9f
mwdziak <mwdziak@gmail.com>
2024-11-18 22:22:49 +0100

    Restored client version with methods UploadMedia and Download Media.

--- chat-service/client/client.go
+++ chat-service/client/client.go
@@ -1,43 +1,90 @@
 package client
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
-type Client struct {
+type MediaServiceClient struct {
 	BaseURL    string
 	HTTPClient *http.Client
 }
 
-func NewClient() (*Client, error) {
-	baseURL := os.Getenv("USER_SERVICE_URL")
+func NewClient() (*MediaServiceClient, error) {
+	baseURL := os.Getenv("MEDIA_SERVICE_URL")
 	if baseURL == "" {
-		return nil, fmt.Errorf("USER_SERVICE_URL environment variable not set")
+		return nil, fmt.Errorf("MEDIA_SERVICE_URL environment variable not set")
 	}
 
-	return &Client{
+	return &MediaServiceClient{
 		BaseURL:    baseURL,
 		HTTPClient: &http.Client{},
 	}, nil
 }
 
-func (c *Client) PostPermissionUpdate() error {
-	req, err := http.NewRequest("POST", c.BaseURL+"/permission-update", nil)
+func (c *MediaServiceClient) UploadMedia(ctx context.Context, roomId string, mediaType string, mediaBytes []byte) (string, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.getMediaURL(roomId, mediaType, ""), nil)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return "", fmt.Errorf("failed to create upload image request: %v", err)
+	}
+
+	log.Printf("Sending request to %s", req.URL.String())
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to send upload image request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return "", fmt.Errorf("unexpected status code from upload image : %d", resp.StatusCode)
+	}
+
+	payload := struct {
+		BlobId string `json:"blobId"`
+	}{
+		BlobId: "",
+	}
+
+	respBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read upload image response: %w", err)
+	}
+
+	if err := json.Unmarshal(respBytes, &payload); err != nil {
+		return "", fmt.Errorf("failed to unmarshal upload image response: %w", err)
+	}
+
+	return payload.BlobId, nil
+}
+
+func (c *MediaServiceClient) DownloadMedia(ctx context.Context, roomId, blobId, mediaType string) ([]byte, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.getMediaURL(roomId, mediaType, blobId), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create download image request: %v", err)
 	}
 
 	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to send request: %w", err)
+		return nil, fmt.Errorf("failed to send dowload image request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code from download image request: %d", resp.StatusCode)
 	}
 
-	return nil
+	return io.ReadAll(resp.Body)
+}
+
+func (c *MediaServiceClient) getMediaURL(roomId, mediaType, blobId string) string {
+	if blobId == "" {
+		return fmt.Sprintf("%s/%s/%s", c.BaseURL, roomId, mediaType)
+	}
+	return fmt.Sprintf("%s/%s/%s/%s", c.BaseURL, roomId, mediaType, blobId)
 }
