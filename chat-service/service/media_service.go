a4d74da3fc8605c665314bdea110a69ba98dd335
mwdziak <mwdziak@gmail.com>
2024-11-18 22:18:47 +0100

    Added media service implementation.

--- chat-service/service/media_service.go
+++ chat-service/service/media_service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"time"
 
 	"example.com/chat_app/chat_service/structs"
+	"github.com/google/uuid"
 )
 
 type MediaRepository interface {
@@ -12,12 +14,45 @@ type MediaRepository interface {
 	SaveFile(ctx context.Context, file *structs.MediaFile) error
 }
 
-type MediaService struct {
-	repo MediaRepository
+type Client interface {
+	UploadMedia(ctx context.Context, roomId string, mediaType string, mediaBytes []byte) (string, error)
+	DownloadMedia(ctx context.Context, blobId string, mediaType string, roomId string) ([]byte, error)
 }
 
-func NewMediaService(repo MediaRepository) *MediaService {
-	return &MediaService{repo: repo}
+type MediaService struct {
+	repo   MediaRepository
+	client Client
+}
+
+func NewMediaService(repo MediaRepository, client Client) *MediaService {
+	return &MediaService{
+		repo:   repo,
+		client: client,
+	}
+}
+
+func (s *MediaService) CreateMediaResource(ctx context.Context, roomId, mediaTypeStr, userId string, mediaBytes []byte) (*structs.MediaFile, error) {
+	blobId, err := s.client.UploadMedia(ctx, roomId, mediaTypeStr, mediaBytes)
+	if err != nil {
+		return nil, err
+	}
+	mediaType, err := structs.ParseMediaType(mediaTypeStr)
+	if err != nil {
+		return nil, err
+	}
+	file := &structs.MediaFile{
+		Id:        uuid.New().String(),
+		RoomId:    roomId,
+		Type:      mediaType,
+		CreatedAt: time.Now(),
+		BlobId:    blobId,
+		CreatedBy: userId,
+	}
+	err = s.repo.SaveFile(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
 }
 
 func (s *MediaService) GetMedia(ctx context.Context, id string) (*structs.MediaFile, error) {
